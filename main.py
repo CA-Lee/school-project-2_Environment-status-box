b9a92dd9ea4864eda51ef283d8c671e609d377c7
CA-Lee <goodidea9487@gmail.com>
2020-03-03 20:28:06 +0800

    ping func is done

--- main.py
+++ main.py
@@ -10,11 +10,21 @@ from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage,
 )
 
+import re
+import time
+
 app = Flask(__name__)
 app.config.from_pyfile('config.cfg')
 
 line_bot_api = LineBotApi(app.config.get('YOUR_CHANNEL_ACCESS_TOKEN'))
 handler = WebhookHandler(app.config.get('YOUR_CHANNEL_SECRET'))
+
+def get_Server_Status():
+    
+    res = "Server status:\n"
+    res += "Now time: " + time.strftime("UTC%z %Y/%m/%d %T") + "\n"
+
+    return res
 
 @app.route("/")
 def root():
@@ -27,7 +37,7 @@ def callback():
 
     # get request body as text
     body = request.get_data(as_text=True)
-    app.logger.info("Request body: " + body)
+    print("Request body: " + body)
 
     # handle webhook body
     try:
@@ -41,10 +51,18 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text="Hello " + event.message.text))
-
+    if re.search("^ping$", event.message.text, re.I):
+        srv_status = get_Server_Status()
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=srv_status)
+        )
+    else:
+        autoreplytext='Command allowed now:\n- Ping'
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text="Hello " + event.message.text + "\n\n" + autoreplytext)
+        )
 
 if __name__ == "__main__":
     app.run()
