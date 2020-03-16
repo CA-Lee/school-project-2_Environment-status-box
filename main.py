331aa07d7cb88f7dbe2919e4d01d6f6ce9bf969f
CA-Lee <goodidea9487@gmail.com>
2020-03-16 10:19:52 +0800

    Minimum prototype including web view page

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, abort
+from flask import Flask, request, abort, render_template
 
 from linebot import (
     LineBotApi, WebhookHandler
@@ -12,11 +12,12 @@ from linebot.models import (
 
 import re
 import time
+import datetime
 import os
 
 import sqlalchemy
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='')
 
 line_bot_api = LineBotApi(os.environ.get('LINE_CHANNEL_ACCESS_TOKEN'))
 handler = WebhookHandler(os.environ.get('LINE_CHANNEL_SECRET'))
@@ -25,13 +26,6 @@ db_user = os.environ.get("DB_USER")
 db_pass = os.environ.get("DB_PASS")
 db_name = os.environ.get("DB_NAME")
 cloud_sql_connection_name = os.environ.get("CLOUD_SQL_CONNECTION_NAME")
-
-def get_Server_Status():
-    
-    res = "Server status:\n"
-    res += "Now time: " + time.strftime("UTC%z %Y/%m/%d %T") + "\n"
-
-    return res
 
 db = sqlalchemy.create_engine(
     # Equivalent URL:
@@ -52,24 +46,73 @@ db = sqlalchemy.create_engine(
 @app.route("/")
 def root():
     return "<center><h1>Hello GAE</h1></center>"
-'''
-    with db.connect() as conn:
-        # Execute the query and fetch all results
-        recent_votes = conn.execute(
-            "SELECT * FROM main_db.app_log;"
-        ).fetchall()
-
-    return str(recent_votes)
-'''
 
 ### Arduino ###
 
-@app.route("/arduino_entry")
+@app.route("/arduino_entry", methods=['POST'])
 def arduino_entry():
     with db.connect() as conn:
-        conn.execute("INSERT INTO `main_db`.`app_log` (`content`) VALUES ('/arduino_entry has been accessed at" + time.strftime("UTC%z %Y/%m/%d %T") + "');")
-    
+        conn.execute("INSERT INTO `main_db`.`app_log` (`content`) VALUES ('/arduino_entry has been accessed at " + time.strftime("UTC%z %Y/%m/%d %T") + "');")
+        
+        keylist = ["source","brightness"]
+        keys = ""
+        values = ""
+        for k in keylist:
+            if keys != "":
+                keys += ", "
+                values += ", "
+            keys += "`" + k + "`"
+            values += "'" + str(request.values.get(k)) + "'"
+
+        conn.execute("INSERT INTO `main_db`.`sensor_data` (" + keys + ") VALUES (" + values + ");")
     return 'OK'
+
+@app.route("/arduino_test")
+def arduino_test():
+    return app.send_static_file('testpage.html')
+
+@app.route("/sensor_data")
+def sensor_data():
+    
+    with db.connect() as conn:
+        # Execute the query and fetch all results
+        data = conn.execute(
+            "SELECT * FROM main_db.sensor_data ORDER BY timestamp desc limit 100;"
+        )
+        raw_timestamps = conn.execute("SELECT timestamp FROM main_db.sensor_data where source='MKR1000' ORDER BY timestamp desc limit 500;").fetchall()
+        timestamps = [row[0].strftime("%X") for row in raw_timestamps]
+        timestamps.reverse()
+        raw_brightnesses = conn.execute("SELECT brightness FROM main_db.sensor_data where source='MKR1000' ORDER BY timestamp desc limit 500;").fetchall()
+        brightnesses = [row[0] for row in raw_brightnesses]
+        brightnesses.reverse()
+        return render_template(
+            "sensor_data.html", 
+            keys=data.keys(),
+            vals=data.fetchone(),
+            timestamps=timestamps,
+            brightnesses=brightnesses
+        )
+
+### Line Bot ###
+
+def get_Server_Status():
+    
+    res = "Server status:\n"
+    res += "Now time: " + time.strftime("UTC%z %Y/%m/%d %T") + "\n"
+
+    return res
+
+def get_newest_data():
+
+    res = ""
+    with db.connect() as conn:
+        res = "The newest data:\n"
+        sql_r = conn.execute("SELECT * FROM main_db.sensor_data ORDER BY timestamp desc limit 1;")
+        col = (sql_r.keys(),sql_r.fetchone())
+        res += str(col[0][0]) + " : " + str(col[1][0] + datetime.timedelta(hours=8)) + "\n"
+        for i in range(1,len(col[0])):
+            res += str(col[0][i]) + " : " + str(col[1][i]) + "\n"
+    return res
 
 @app.route("/callback", methods=['POST'])
 def callback():
@@ -91,18 +134,17 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+    reply = ""
+    
     if re.search("^ping$", event.message.text, re.I):
-        srv_status = get_Server_Status()
-        line_bot_api.reply_message(
-            event.reply_token,
-            TextSendMessage(text=srv_status)
-        )
-    else:
-        autoreplytext='Command allowed now:\n- Ping'
-        line_bot_api.reply_message(
-            event.reply_token,
-            TextSendMessage(text="Hello " + event.message.text + "\n\n" + autoreplytext)
-        )
+        reply = get_Server_Status()
+    elif re.search("^get[\s_]now$", event.message.text, re.I):
+        reply = get_newest_data()
+    
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=reply)
+    )
 
 if __name__ == "__main__":
     app.run()
